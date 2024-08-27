9614304bc68c1c072a25ff723904a3bf814b5d2d
ArafatBytes <mdarafat1661@gmail.com>
2024-08-27 18:38:28 +0600

    Update new projects

--- src/components/PfData.js
+++ src/components/PfData.js
@@ -1,34 +1,34 @@
 import quiz from "../assets/quiz.png";
 import calculator from "../assets/calculator.png";
 import recipe from "../assets/recipe.png";
-import landing from "../assets/landing.png";
 import sketch from "../assets/sketch.png";
-import rps from "../assets/rps.png";
+import resort from "../assets/resort.png";
+import business from "../assets/business.png";
 
 export const projectsData = [
   {
     id: 1,
+    image: resort,
+    title: "Resort website",
+    link: "https://blahblahhs.my.canva.site/palm-paradise#home",
+  },
+  {
+    id: 2,
+    image: business,
+    title: "Business website",
+    link: "https://blahblahhs.my.canva.site/#home",
+  },
+  {
+    id: 3,
     image: quiz,
     title: "Quiz App",
     link: "https://bright-buttercream-117a88.netlify.app/",
   },
   {
-    id: 2,
+    id: 4,
     image: calculator,
     title: "Calculator Project",
     link: "https://arafatbytes.github.io/Brain-Calculator/",
-  },
-  {
-    id: 3,
-    image: recipe,
-    title: "Recipe Solution",
-    link: "https://arafatbytes.github.io/Odin-Recipes/",
-  },
-  {
-    id: 4,
-    image: landing,
-    title: "Landing Page",
-    link: "https://arafatbytes.github.io/Mystery/",
   },
   {
     id: 5,
@@ -38,8 +38,8 @@ export const projectsData = [
   },
   {
     id: 6,
-    image: rps,
-    title: "Rock Paper Scissor",
-    link: "https://arafatbytes.github.io/Rock-Paper-Scissor/",
+    image: recipe,
+    title: "Recipe Solution",
+    link: "https://arafatbytes.github.io/Odin-Recipes/",
   },
 ];
